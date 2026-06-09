c4b060c2a601fb92a90c6b974686f4b62698c1c5
Oliver Sande <oliversande@gmail.com>
2026-06-09 11:46:18 +0200

    feat: add function for getting a openai client

---
+++ src/hackathon/autogen_client.py
@@ -0,0 +1,35 @@
+# pip install -U "autogen-agentchat" "autogen-ext[openai]"
+import asyncio
+
+from autogen_agentchat.agents import AssistantAgent
+from autogen_core.models import ModelInfo
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+
+
+def get_client():
+    base_url = "https://hackerton2026.compute.dtu.dk/v1"
+    api_key = "REDACTED"
+
+    client = OpenAIChatCompletionClient(
+        model="google/gemma-4-26b-a4b",
+        api_key=api_key,
+        base_url=base_url,
+        model_info=ModelInfo(
+            vision=False,
+            function_calling=True,
+            json_output=True,
+            family="unknown",
+            structured_output=True,
+        ),
+    )
+    return client
+
+
+async def hello_world() -> None:
+    client = get_client()
+    agent = AssistantAgent("assistant", client, system_message="")
+    print(await agent.run(task="Say 'Hello World!'"))
+
+
+if __name__ == "__main__":
+    asyncio.run(hello_world())
