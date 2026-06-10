7f56e4c5e7e51e24904bb8e07341d550797c86b1
Oliver Sande <oliversande@gmail.com>
2026-06-10 14:01:07 +0200

    feat: add api key using pydantic-settings

--- src/hackathon/autogen_client.py
+++ src/hackathon/autogen_client.py
@@ -5,16 +5,17 @@ from autogen_agentchat.agents import AssistantAgent
 from autogen_core.models import ModelInfo
 from autogen_ext.models.openai import OpenAIChatCompletionClient
 
+from hackathon.settings import settings
+
 
 def get_client() -> OpenAIChatCompletionClient:
     base_url = "https://hackerton2026.compute.dtu.dk/v1"
-    api_key = "REDACTED"
 
     client = OpenAIChatCompletionClient(
         model="google/gemma-4-26b-a4b",
         # model="alibaba/qwen-3.6-35b-a3b",
-        api_key=api_key,
         base_url=base_url,
+        api_key=settings.llm_token,
         model_info=ModelInfo(
             vision=False,
             function_calling=True,
